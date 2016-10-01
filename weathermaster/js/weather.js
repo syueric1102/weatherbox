9180c7e1896ed943f89a3039d2ee44e89231c114
Eric <ejsyu@ucsd.edu>
2016-10-01 19:21:47 +0000

    fix

--- weathermaster/js/weather.js
+++ weathermaster/js/weather.js
@@ -24,7 +24,7 @@ function loadWeather(location, woeid){
             city = weather.city;
             temp = weather.temp + '&deg;';
             wcode = '<img class="weathericon" src="weathermaster/images/weathericons/'+weather.code+'.svg">';
-            if (weather.code == 33){
+            if (weather.code == 36){
                 var request = new XMLHttpRequest();
                 request.open( "GET" , "gpiotestone.php", true);
                 request.send(null);
