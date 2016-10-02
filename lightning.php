c424e4aece8e3217ef5f0928080bacf4e2408d56
ericsyu1102 <syueric1102@gmail.com>
2016-10-02 07:13:47 -0700

    aosdifjio

--- lightning.php
+++ lightning.php
@@ -3,7 +3,7 @@
 	system("gpio mode 2 out");
 	system("gpio mode 3 out");
 	system("gpio write 2 1");
-	system("gpio.write 3 on");
+	system("gpio write 3 0");
 	for($i = 0; $i = 2; $i++){
 		system("gpio write 1 1");
 		sleep(0.3);
@@ -14,6 +14,7 @@
 		system("gpio write 1 0");
 		sleep(4);
 	}
-	system("gpio.write 2 0");
-	system("gpio write 3 0")
+	system("gpio write 1 0");
+	system("gpio write 2 0");
+	system("gpio write 3 0");
 ?>
