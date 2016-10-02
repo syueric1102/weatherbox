61a30eaf852a3bcfbfb7e8f19a69ca986d0c3435
Eric <ejsyu@ucsd.edu>
2016-10-02 14:42:12 +0000

    aoidjf

--- lightning.php
+++ lightning.php
@@ -1,18 +1,19 @@
 <?php
-	system("gpio mode 1 out");
+	system("gpio mode 6 out");
 	system("gpio mode 2 out");
 	system("gpio mode 3 out");
 	system("gpio write 2 1");
 	system("gpio write 3 1");
-	system("gpio write 1 1");
+	system("gpio write 6 1");
 	sleep(0.3);
-	system("gpio write 1 0");
+	system("gpio write 6 0");
 	sleep(0.3);
-	system("gpio write 1 1");
+	system("gpio write 6 1");
 	sleep(0.3);
-	system("gpio write 1 0");
+	system("gpio write 6 0");
 	sleep(4);
-	system("gpio write 1 0");
+	system("gpio write 6 0");
 	system("gpio write 2 0");
 	system("gpio write 3 0");
+	system("GPIO.cleanup()");
 ?>
--- rainycloudy.php
+++ rainycloudy.php
@@ -2,6 +2,7 @@
         system("gpio mode 2 out");
 	system("gpio mode 3 out");
         system("gpio write 3 1");
+        system("gpio write 2 1");
         sleep(7);
         system("gpio write 2 0");
 	system("gpio write 3 0");
